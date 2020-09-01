54b781bd182a26299b9190ecb9bad374dcbabc63
Madiyor <abdukhashimov@yandex.ru>
2020-09-01 18:23:13 +0500

    Added

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -1,6 +1,4 @@
-package main
-
-import "fmt"
+// package main
 
 /*
  * Write a short function that prints each number from 0 to 100 on a new line
@@ -9,16 +7,16 @@ import "fmt"
  * For each multiple of 3 and 5 print the word fizzbuzz
  */
 
-func main() {
-	for i := 1; i < 100; i++ {
-		if i%15 == 0 {
-			fmt.Println("Fizz")
-		} else if i%3 == 0 {
-			fmt.Println("Buzz")
-		} else if i%5 == 0 {
-			fmt.Println("FizzBuzz")
-		} else {
-			fmt.Println(i)
-		}
-	}
-}
+// func main() {
+// 	for i := 1; i < 100; i++ {
+// 		if i%15 == 0 {
+// 			fmt.Println("Fizz")
+// 		} else if i%3 == 0 {
+// 			fmt.Println("Buzz")
+// 		} else if i%5 == 0 {
+// 			fmt.Println("FizzBuzz")
+// 		} else {
+// 			fmt.Println(i)
+// 		}
+// 	}
+// }
