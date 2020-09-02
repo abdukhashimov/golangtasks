0e2e2ab7edc48267da4b91279b395a78eab0052b
Madiyor <abdukhashimov@yandex.ru>
2020-09-02 06:05:39 +0500

    defined tasks

--- structs.go
+++ structs.go
@@ -1,13 +1,17 @@
-struct Tasks {
-    id int
-    name int
-    done bool
+package main
+
+import "fmt"
+
+type tasks struct {
+	id   int
+	name string
+	done bool
 }
 
-func (t Tasks) display() {
-    
+func (t tasks) display() {
+	fmt.Println("ID: ", t.id, "Name: ", t.name, "Done: ", t.done)
 }
 
 func main() {
-    
+
 }
