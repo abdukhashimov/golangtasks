27c88f4dd5c39ae4c036f00557851819ca308c9e
Madiyor <abdukhashimov@yandex.ru>
2020-09-02 07:28:23 +0500

    learned concurrent go

---
+++ gocunc.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		count("laptop")
+		wg.Done()
+	}()
+	wg.Wait()
+}
+
+func count(thing string) {
+	for i := 0; i < 5; i++ {
+		fmt.Println(i, thing)
+		time.Sleep(time.Millisecond * 500)
+	}
+}
